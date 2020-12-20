30e05b58466bcbac8edb0e9298f3c60489e697d9
tk0929 <bluecoffin929@gmail.com>
2020-12-20 20:02:41 +0900

    [add] json解析用のstructの追加

---
+++ GurunabiSampleApp/Model/GurunabiRestaurantData.swift
@@ -0,0 +1,19 @@
+//
+//  GurunabiRestruntData.swift
+//  GurunabiSampleApp
+//
+//  Created by t.koike on 2020/12/20.
+//
+
+import Foundation
+
+struct GurunabiRestaurantData {
+    
+    var latitude: String?
+    var longitude: String?
+    var url: String?
+    var name: String?
+    var tel: String?
+    var restaurantImage: String?
+    
+}
